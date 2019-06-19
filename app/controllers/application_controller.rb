edba204d57ba2e8c7c06b11c95bc36d5792b990c
Amee Covarrubias <ameecova@yahoo.com>
2019-06-19 23:21:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,6 +36,7 @@ class ApplicationController < Sinatra::Base
   patch "/articles/:id" do
     @article = Article.find(params[:id])
     @article.update(:content => params[:content])
+    binding.pry
     redirect "/articles/#{@article.id}"
   end
   
